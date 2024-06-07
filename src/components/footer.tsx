824bdcafce9c67912f58968509c9c60ecebfbcb4
Kyle Montag <thekylemontag@gmail.com>
2024-06-06 20:07:53 -0400

    simple icons

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { Separator } from "@/components/ui/separator";
+import { SiGithub, SiX } from "@icons-pack/react-simple-icons";
 
 export default function Footer() {
   return (
@@ -7,13 +8,10 @@ export default function Footer() {
       <Separator className="my-4" />
       <div className="flex justify-center space-x-4">
         <Link href="https://github.com/kmontag42" target="_blank">
-          GitHub
+          <SiGithub />
         </Link>
         <Link href="https://x.com/thekylemontag" target="_blank">
-          Twitter
-        </Link>
-        <Link href="https://linkedin.com" target="_blank">
-          LinkedIn
+          <SiX />
         </Link>
       </div>
     </footer>
